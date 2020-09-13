3bf979b08aeceab9658d798754e7e4e1ff5aae9b
Muhammad Mustajab Gohar <muhammadmustajabgohar@gmail.com>
2020-09-13 14:50:50 +0500

    routes with postman

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
-
 const app = express();
+
+//Get Database
+const connectDb = require('./config/db.js');
+
+//Connect Database
+connectDb();
 
 app.get('/', (req, res) => {
   res.send('API Running');
 });
+
+// Use Routes here
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
 
 const PORT = process.env.PORT || 5000;
 
